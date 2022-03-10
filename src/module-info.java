447d75ac88d9cabc31fc9e16de35d1fa31aab125
Jonatas Melo <jonnmello@hotmail.com>
2022-03-10 17:44:40 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module javafxjdbc {
 	 requires javafx.controls;
 
      requires javafx.fxml;
+	requires mysql.connector.java;
+	requires java.sql;
 
      opens application to javafx.graphics, javafx.fxml;
 
